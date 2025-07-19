85428770c14ff3b70b322abdf1bc78ddbc311143
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-07-19 20:46:11 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,6 @@
 # exit on error
 set -o errexit
 
-# NEW SECTION: Install system-level dependencies
-# -----------------------------------------------
-# This is for packages like lxml that need to be compiled
-# and require system C libraries and build tools.
-apt-get update
-apt-get install -y build-essential libxml2-dev libxslt1-dev
-
-# --- Your original build commands ---
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input

859571ae43985bf941737e1290a2b3efec3f2c83
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-07-20 14:31:39 +0300

    feat: Migrate project to pipenv for robust deployment

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# This script tells Render how to build your app.
 
+# 1. Install pipenv
+pip install pipenv
+
+# 2. Use pipenv to install all dependencies from Pipfile.lock
+pipenv install --deploy --system
+
+# 3. Run your standard Django production commands
 python manage.py collectstatic --no-input
 python manage.py migrate

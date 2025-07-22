368283e1bc7811a34cbaa03584c5652883a385cb
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-07-22 04:45:38 +0300

    Fix: Add djoser to requirements for Render deploy

--- farmart_backend/settings.py
+++ farmart_backend/settings.py
@@ -127,8 +127,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
     )
 }
+
 
 
 if IS_DEVELOPMENT:
